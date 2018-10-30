50774411fe86cc2e46c8d01beb674003a8733480
vasil <vasildimitrovhristov@gmail.com>
2018-10-30 06:43:24 +0200

    serve static assets

--- server/index.js
+++ server/index.js
@@ -14,6 +14,15 @@ app.use(bodyParser.json());
 
 app.use('/api/products', productsRoute);    
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 const port = process.env.PORT || 5000;
 app.listen(port , () => {
     console.log(`Server is running on port ${port}`);
