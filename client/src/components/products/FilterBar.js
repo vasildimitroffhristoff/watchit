37e076704882849faa057d7c664a377149f69d44
vasil <vasildimitrovhristov@gmail.com>
2018-10-31 07:28:27 +0200

    Added filter bar for tags

--- client/src/components/products/FilterBar.js
+++ client/src/components/products/FilterBar.js
@@ -39,7 +39,7 @@ class FilterBar extends Component {
       <div className="card border-0 rounded-0 shadow-lg">
           <div className="card-body">
             <h5 className="mb-3">Filter results:</h5>
-            <div className="mb-3">
+            <div className="mb-4">
               <label className="d-block mb-3">
                 <small>
                 <b>Filter by price:</b> <span className="text-dark pl-1">$ { volume } </span>
@@ -56,12 +56,30 @@ class FilterBar extends Component {
                 step="10" />
             </div>
           
-          <div>
+            <div className="mb-4">
               <label><small className="font-weight-bold">Sort by</small></label>
                 <select onChange={this.handleSort} name="priceselect" className="w-100 border rounded-0 form-control">
                   <option defaultValue name="" value="lowest">Lowest Price</option>
                   <option name="" value="highest">Highest Price</option>
                 </select>
+            </div>
+
+            <div className="mb-2">
+            <div>
+              <label><small className="font-weight-bold">Filter by tags</small></label>
+                
+            </div>
+            <div className="btn-group btn-group-toggle" data-toggle="buttons">
+              <label className="btn btn-light btn-sm active">
+                <input type="radio" name="options" id="option1" autocomplete="off" checked /> Suuton
+              </label>
+              <label className="btn btn-light btn-sm">
+                <input type="radio" name="options" id="option2" autocomplete="off" /> Garmin
+              </label>
+              <label className="btn btn-light btn-sm">
+                <input type="radio" name="options" id="option3" autocomplete="off" /> Polar
+              </label>
+            </div>
             </div>
               <button className="btn btn-primary w-100 mt-3 d-block rounded-0" onClick={this.applyFilters}>Apply Filter</button>
           </div>
