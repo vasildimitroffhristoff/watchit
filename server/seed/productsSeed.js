e5344d0af0a83b7fcda5083a4b26f818c14233b1
vasil <vasildimitrovhristov@gmail.com>
2018-11-04 10:50:27 +0200

    Fixes and features: cloudinary, dont close cart on click inside dropdown, handle product if it is already in cart

--- server/seed/productsSeed.js
+++ server/seed/productsSeed.js
@@ -11,61 +11,67 @@ const products = [
     new Product({
         name: "Suunto 25G",
         price: 145,
-        image: "https://cdn.pixabay.com/photo/2015/06/25/17/22/smart-watch-821559_960_720.jpg",
-        category: "suunto"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315274/watchit/smart-watch-821559_960_720.jpg",
+        category: "suunto",
+        featured: true
     }),
     new Product({
         name: "Suunto XI",
         price: 495,
-        image: "https://cdn.pixabay.com/photo/2015/07/02/10/29/smartwatch-828786_960_720.jpg",
-        category: "suunto"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315274/watchit/smart-watch-821557_960_720.jpg",
+        category: "suunto",
+        featured: true
     }),
     new Product({
         name: "Suunto GF",
         price: 219,
-        image: "https://cdn.pixabay.com/photo/2015/06/25/17/21/smart-watch-821557_960_720.jpg",
-        category: "suunto"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315274/watchit/smart-watch-889639_960_720.jpg",
+        category: "suunto",
+        featured: true
     }),
     new Product({
         name: "Garmin XF",
         price: 239,
-        image: "https://cdn.pixabay.com/photo/2015/06/25/17/22/smart-watch-821563_960_720.jpg",
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315274/watchit/smartwatch-828786_960_720.jpg",
         category: "garmin"
     }),
     new Product({
         name: "Garmin XG30",
         price: 329,
-        image: "https://cdn.pixabay.com/photo/2017/02/12/12/54/polar-a360-2059937_960_720.jpg",
-        category: "garmin"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315274/watchit/watch-2910920_960_720.jpg",
+        category: "garmin",
+        featured: true
     }),
     new Product({
         name: "Garmin XG30",
         price: 429,
-        image: "https://cdn.pixabay.com/photo/2015/08/15/15/21/smart-watch-889639_960_720.jpg",
-        category: "garmin"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315273/watchit/smart-watch-821563_960_720.jpg",
+        category: "garmin",
+        featured: true
     }),
     new Product({
         name: "Suunto X0",
         price: 199,
-        image: "https://cdn.pixabay.com/photo/2017/12/04/06/00/watch-2996385_960_720.jpg",
-        category: "suunto"
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315273/watchit/watch-2996385_960_720.jpg",
+        category: "suunto",
+        featured: true
     }),
     new Product({
         name: "Suunto X++",
         price: 259,
-        image: "https://cdn.pixabay.com/photo/2017/07/31/21/59/apple-2561475_960_720.jpg",
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315273/watchit/apple-916402_960_720.jpg",
         category: "suunto"
     }),
     new Product({
         name: "Suunto XG30",
         price: 329,
-        image: "https://cdn.pixabay.com/photo/2017/11/02/12/03/watch-2910920_960_720.jpg",
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315273/watchit/apple-2561475_960_720.jpg",
         category: "suunto"
     }),
     new Product({
         name: "Polar XX",
         price: 329,
-        image: "https://cdn.pixabay.com/photo/2015/09/01/03/42/apple-916402_960_720.jpg",
+        image: "https://res.cloudinary.com/dctggpdo6/image/upload/v1541315273/watchit/polar-a360-2059937_960_720.jpg",
         category: "polar"
     })
 ]    
